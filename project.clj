3e5901073a9fb6b85b901b4db7b8dca3f4548120
nivekuil <mail@nivekuil.com>
2021-08-17 18:36:08 -0700

    add deps, bump jsch version

--- project.clj
+++ project.clj
@@ -4,15 +4,15 @@
   :description "Library for using SSH from clojure."
   :url "https://github.com/clj-commons/clj-ssh"
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/tools.logging "0.2.6"
                   :exclusions [org.clojure/clojure]]
-                 [com.jcraft/jsch.agentproxy.usocket-jna ~agentproxy-version]
-                 [com.jcraft/jsch.agentproxy.usocket-nc ~agentproxy-version]
-                 [com.jcraft/jsch.agentproxy.sshagent ~agentproxy-version]
-                 [com.jcraft/jsch.agentproxy.pageant ~agentproxy-version]
-                 [com.jcraft/jsch.agentproxy.core ~agentproxy-version]
-                 [com.jcraft/jsch.agentproxy.jsch ~agentproxy-version]
-                 [com.github.mwiede/jsch "0.1.64"]]
+                 [com.jcraft/jsch.agentproxy.usocket-jna "0.0.9"]
+                 [com.jcraft/jsch.agentproxy.usocket-nc "0.0.9"]
+                 [com.jcraft/jsch.agentproxy.sshagent "0.0.9"]
+                 [com.jcraft/jsch.agentproxy.pageant "0.0.9"]
+                 [com.jcraft/jsch.agentproxy.core "0.0.9"]
+                 [com.jcraft/jsch.agentproxy.jsch "0.0.9"]
+                 [com.github.mwiede/jsch "0.1.65"]]
   :jvm-opts ["-Djava.awt.headless=true"]
   :profiles {:provided {:dependencies [[org.clojure/clojure "1.10.1"]]}})
